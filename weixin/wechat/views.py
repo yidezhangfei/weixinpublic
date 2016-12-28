b32f4bc3531ced531525c6ae0cd5d721333707fa
李军 <lijunSI0413@sogou-inc.com>
2016-12-28 20:24:04 +0800

    add lxml

--- weixin/wechat/views.py
+++ weixin/wechat/views.py
@@ -2,11 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.utils.encoding import smart_str
+from django.views.decorators.csrf import csrf_exempt
+from lxml import etree
+import json
 import hashlib
 
+TOKEN = "gyn"
+
+@csrf_exempt
 def response(request):
-    signature = request.GET.get('signature', None)
-    timestamp = request.GET.get('tiemstamp', None)
-    nonce = request.GET.get('nonce', None)
-    echostr = request.GET.get('echostr', None)
-    return HttpResponse(echostr)
+    if request.method == "GET":
+        signature = request.GET.get('signature', None)
+        timestamp = request.GET.get('timestamp', None)
+        nonce = request.GET.get('nonce', None)
+        token = TOKEN
+        tmp_list = [token, timestamp, nonce]
+        tmp_list.sort()
+        tmp_str = "%s%s%s" % tuple(tmp_list)
+        tmp_str = hashlib.sha1(tmp_str).hexdigest()
+        if tmp_str == signature:
+            return HttpResponse(echostr)
+        else:
+            return HttpResponse("error")
+    else:
+        xml_str = smart_str(request.body)
+        request_xml = etree.fromstring(xml_str)
+        response_str = request_xml
+    return HttpResponse(response_str)
